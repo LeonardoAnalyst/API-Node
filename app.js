55122d39f5414bb382bb23fe4db9d89583c2ed81
Leozera <leobhal13@gmail.com>
2019-09-18 13:48:43 -0300

    Finalizando o CRUD da nossa API

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ app = express();
 
 //Conexao MongoDB
 
-mongoose.connect('mongodb+srv://root:root@cluster0-84qfi.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://"+srv+"://root:root@cluster0-84qfi.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
+},(err)=>{
+console.log(`Erro ao conectar ${err}`);
+
 });
 
 app.use(bodyParser());
@@ -18,12 +21,21 @@ var port = process.env.PORT || 3000;
 
 // Rotas 
 
-var route = express.Router();
-route.get('/', (req, res) => {
+var router = express.Router();
+router.get('/', (req, res) => {
     res.json({ messae: 'API CodeZera' });
 });
 
-route.Router('/times')
+router.route('/times')
+    .get((req, res) => {
+        Clubes.find((err, dados) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(dados)
+            }
+        })
+    })
     .post((req, res) => {
         var clubes = new Clubes();
         clubes.nome = req.body.nome;
@@ -31,14 +43,50 @@ route.Router('/times')
             if (err) {
                 res.send(err);
             } else {
-                res.json({ message: 'Time cadastrado com sucesso!' })
+                res.json({ message: 'Time cadastrado com sucesso!' });
             }
-        })
+        });
     });
 
-app.use('/api', route);
+router.route('/times/:id')
+    .get((req, res) => {
+        Clubes.findById(req.params.id, (err, dados) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(dados);
+            }
+        });
+    })
+    .put((req, res) => {
+        Clubes.findById(req.params.id, (err, dados) => {
+            if (err) {
+                res.send(err);
+            } else {
+                dados.nome = req.body.nome;
+                dados.save((err) => {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        res.json({ message: 'Time atualizado com sucesso!' })
+                    }
+                });
+            }
+        });
+    })
+    .delete((req, res) => {
+        Clubes.remove({ _id: req.params.id }, (err, dados) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json({ message: 'Time excluido com sucesso!' });
+            }
+        });
+    });
+
+app.use('/api', router);
 
 app.listen(port, () => {
-    console.log('Servidor rodando na porta ' + port);
+    console.log(`Servidor rodando na porta ${port}`);
 });
 
